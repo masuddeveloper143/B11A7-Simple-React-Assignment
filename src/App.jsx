5698ce9c05ee3fe2a0ad9598e5b3eb74f81dbbf5
Masud Rana <md.masudrana.developer@gmail.com>
2026-04-09 21:31:56 +0800

    Update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <h1>B11A7-Simple-React-Assignment</h1>
+      <h1 className='text-5xl font-bold text-center text-red-500'>B11A7-Simple-React-Assignment</h1>
+
+      <div className='main-container flex gap-2 text-center'>
+        <div className='right-box w-[70%] border-2'>right</div>
+        <div className='left-box w-[30%] border-2'>left</div>
+      </div>
     </>
   )
 }
